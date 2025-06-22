81b70bc86c0b48adc17959318951f30e3e900f23
Venkata-Guru-Mahesh <130880135+Venkata-Guru-Mahesh@users.noreply.github.com>
2025-06-22 15:37:09 +0530

    Update Product.java

--- Week-1/Data_Structures_and_Algorithms/E-commerce_Platform_Search_Function/codes/Product.java
+++ Week-1/Data_Structures_and_Algorithms/E-commerce_Platform_Search_Function/codes/Product.java
@@ -1,8 +1,6 @@
 public class Product {
-    @SuppressWarnings("unused")
     int productId;
     String productName;
-    @SuppressWarnings("unused")
     String category;
 
     public Product(int productId, String productName,String category) {
