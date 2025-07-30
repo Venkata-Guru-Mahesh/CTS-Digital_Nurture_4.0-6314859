38a890dd85bac79f3f7e2ef1681c3f61bb721448
Mahesh <yelisettymahesh@gmail.com>
2025-07-30 12:05:29 +0530

    added reactjs-5th file

---
+++ Week-6/5. ReactJS-HOL/Code/cohorttracker/src/CohortDetails.js
@@ -0,0 +1,28 @@
+import styles from './CohortDetails.module.css';
+
+function CohortDetails(props) {
+    let statusClass = styles.statusOther
+    if (props.cohort.currentStatus === 'Ongoing'){
+        statusClass = styles.statusOngoing
+    }
+
+    return (
+        <div className={styles.box}>
+            <h3 className={statusClass}>
+                {props.cohort.cohortCode} - <span>{props.cohort.technology}</span>
+            </h3>
+            <dl>
+                <dt>Started On</dt>
+                <dd>{props.cohort.startDate}</dd>
+                <dt>Current Status</dt>
+                <dd>{props.cohort.currentStatus}</dd>
+                <dt>Coach</dt>
+                <dd>{props.cohort.coachName}</dd>
+                <dt>Trainer</dt>
+                <dd>{props.cohort.trainerName}</dd>
+            </dl>
+        </div>
+    );
+}
+
+export default CohortDetails;
